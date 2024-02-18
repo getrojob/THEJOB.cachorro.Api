0739b050125efb9f334c2cab4bc5aae49ad3819c
Getulio Rodriguez <getrojob@gmail.com>
2024-02-18 11:48:58 -0300

    Feature/app configuration (#38)
    * feat: add appconfiguration
    
    * feat: add appconfiguration

---
+++ src/THEJOB.Cachorro.Api/Configs/Settings.cs
@@ -0,0 +1,10 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace THEJOB.Cachorro.Api.Configs
+{
+    [ExcludeFromCodeCoverage]
+    public class Settings
+    {
+        public string? ValorDaMensagem { get; set; }
+    }
+}
